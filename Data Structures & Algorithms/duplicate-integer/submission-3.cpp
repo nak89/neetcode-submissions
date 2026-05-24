9cccf3dfd102e8e79120c087b1045dd3c8cb2d2c
nak89 <mongkulratanak013@gmail.com>
2026-05-24 22:22:17 +1000

    Bulk sync: 4 submissions

---
+++ Data Structures & Algorithms/duplicate-integer/submission-3.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    bool hasDuplicate(vector<int>& nums) {
+    unordered_set<int> seen; 
+    for (int i = 0; i < nums.size(); i++){
+    if(seen.find(nums[i]) == seen.end()){
+    seen.insert(nums[i]);
+    }
+    else return true;
+    }
+    return false;
+    }
+};
